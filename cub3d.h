3894597e078c711f0e380e32ba7f257820d57a9c
juhanse <julien.hanse12@gmail.com>
2025-04-20 18:03:54 +0200

    add: structure

--- cub3d.h
+++ cub3d.h
@@ -2,6 +2,23 @@
 # define CUB3D_H
 
 # include <stdio.h>
+# include <stdlib.h>
 # include <string.h>
+# include <math.h>
+
+typedef enum e_move
+{
+	RIGHT,
+	LEFT,
+	UP,
+	DOWN,
+	ROT_RIGHT,
+	ROT_LEFT
+}	t_move;
+
+typedef struct s_data
+{
+	char	**map;
+}	t_data;
 
 #endif
