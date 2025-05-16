32965df813c5dac1bbb7cf5f5040eac4e0a8def4
juhanse <julien.hanse12@gmail.com>
2025-05-17 00:28:25 +0200

    fix: found player position

--- srcs/init/parse_map.c
+++ srcs/init/parse_map.c
@@ -2,33 +2,45 @@
 
 int	ft_found_player(t_data *data)
 {
-	int	i;
-	int	j;
+	int		i;
+	size_t	j;
+	int		found;
 
 	i = -1;
+	found = 0;
 	while (++i < data->height)
 	{
 		j = -1;
-		while (++j < data->width)
-			if (data->map[i][j] == 'N' || data->map[i][j] == 'S' \
-			|| data->map[i][j] == 'E' || data->map[i][j] == 'W')
-				return (perror(ERR_PLY_NOT_FOUND), 1);
+		while (++j < ft_strlen(data->map[i]))
+		{
+			if (ft_strchr("NSEW", data->map[i][j]))
+			{
+				data->player->x = j;
+				data->player->y = i;
+				data->player->angle = data->map[i][j];
+				found = 1;
+			}
+		}
 	}
+	if (!found)
+		return (perror(ERR_PLY_NOT_FOUND), 1);
 	return (0);
 }
 
 int	ft_check_walls(t_data *data)
 {
-	int	i;
-	int	j;
+	int		i;
+	size_t	j;
+	size_t	len;
 
 	i = -1;
 	while (++i < data->height)
 	{
+		len = ft_strlen(data->map[i]);
 		j = -1;
-		while (++j < data->width)
+		while (++j < len)
 			if (i == 0 || i == data->height - 1 || j == 0 \
-				|| j == data->width - 1)
+				|| j == len - 1)
 				if (data->map[i][j] != '1')
 					return (perror(ERR_WALLS), 1);
 	}
@@ -37,14 +49,14 @@ int	ft_check_walls(t_data *data)
 
 int	ft_check_char(t_data *data)
 {
-	int	i;
-	int	j;
+	int		i;
+	size_t	j;
 
 	i = -1;
 	while (++i < data->height)
 	{
 		j = -1;
-		while (++j < data->width)
+		while (++j < ft_strlen(data->map[i]))
 			if (data->map[i][j] != '0' && data->map[i][j] != '1' \
 			&& data->map[i][j] != 'N' && data->map[i][j] != 'S' \
 			&& data->map[i][j] != 'E' && data->map[i][j] != 'W')
