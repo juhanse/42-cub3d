b9a20a254ab963cf013230099fdfa58301e451b8
juhanse <julien.hanse12@gmail.com>
2025-05-16 23:56:50 +0200

    fix: prototypes declarations

--- cub3d.h
+++ cub3d.h
@@ -11,11 +11,12 @@
 # include "./libft/gnl/gnl.h"
 // #include "./mlx/include/MLX42/MLX42.h"
 
-# define ERR_BAD_PATH 	"Error\nBad path map\n"
-# define ERR_ARGS 		"Error\nInvalid arguments\n"
-# define ERR_SQUARE 	"Error\nInvalid square map\n"
-# define ERR_WALLS 		"Error\nMissing walls map\n"
-# define ERR_CHAR_MAP	"Error\nInvalid characters map\n"
+# define ERR_BAD_PATH 		"Error\nBad path map\n"
+# define ERR_ARGS 			"Error\nInvalid arguments\n"
+# define ERR_SQUARE 		"Error\nInvalid square map\n"
+# define ERR_WALLS 			"Error\nMissing walls map\n"
+# define ERR_CHAR_MAP		"Error\nInvalid characters map\n"
+# define ERR_PLY_NOT_FOUND	"Error\nPlayer not found\n"
 
 typedef enum e_move
 {
@@ -54,13 +55,17 @@ typedef struct s_data
 // UTILS
 void	ft_debug(t_data *data);
 void	ft_exit(char *msg);
+void	ft_free_map(t_data *data);
+void	ft_print_map(char **map);
 
-// PARSING
+// INIT
+int		ft_initialize(t_data *data, char *path);
 void	ft_fill_content(t_data *data);
 void	ft_get_config_color(t_data *data);
 void	ft_get_config_texture(t_data *data);
-void	ft_get_map_from_content(t_data *data);
-void	ft_validate_map(char **map);
-int		ft_initialize(t_data *data, char *path);
+void	ft_get_map(t_data *data);
+int		ft_found_player(t_data *data);
+int		ft_check_walls(t_data *data);
+int		ft_check_char(t_data *data);
 
 #endif
