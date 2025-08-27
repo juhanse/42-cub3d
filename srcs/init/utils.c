765f2c990d032336224e9ac9a92ce6d78267fe05
juhanse <julien.hanse12@gmail.com>
2025-08-27 16:06:13 +0200

    delete: print map

--- srcs/init/utils.c
+++ srcs/init/utils.c
@@ -27,27 +27,16 @@ void	ft_free_split(char **s)
 	free(s);
 }
 
-void	ft_print_map(t_data *data)
-{
-	int	i;
-
-	i = -1;
-	printf("\n------ MAP ------\n");
-	while (++i < data->map_height)
-		printf("[%2d] %s\n", i, data->map[i]);
-	printf("-----------------\n");
-}
-
 void	ft_debug(t_data *data)
 {
 	int	i;
 
 	i = -1;
-	printf("MAP COPY LEN: %d\n", data->map_copy_height);
-	while (++i < data->map_copy_height)
-		printf("[%d] %s\n", i, data->map_copy[i]);
+	printf("MAP HEIGHT: %d\n", data->map_height);
+	while (++i < data->map_height)
+		printf("[%d] %s\n", i, data->map[i]);
 	printf("\nNO: %s\nSO: %s\nWE: %s\nEA: %s\n", data->north_image, \
 		data->south_image, data->west_image, data->east_image);
-	printf("\nMAP_HEIGHT: %d", data->map_height);
-	ft_print_map(data);
+	printf("\nFLOOR: [%d]\nCEILING: [%d]\n", data->floor_color, \
+		data->ceiling_color);
 }
