8742f5d3ae20f598f57102ad8ed4d06752b1892b
juhanse <julien.hanse12@gmail.com>
2025-08-27 17:06:31 +0200

    add: get map function

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -23,6 +23,7 @@ int	ft_initialize(t_data *data, char *path)
 		return (ft_free_map(data), 1);
 	ft_get_config_texture(data);
 	ft_get_config_color(data);
+	ft_get_map(data);
 	ft_debug(data);
 	if (!ft_check_char(data) || !ft_found_player(data) \
 	|| !ft_check_walls(data))
