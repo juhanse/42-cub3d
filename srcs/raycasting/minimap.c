fb572e95c5260b1790c196d1495d62aaf1e87029
Antonia <a.dewoelmont@gmail.com>
2025-08-28 15:18:03 +0200

    fix: centered player

--- srcs/raycasting/minimap.c
+++ srcs/raycasting/minimap.c
@@ -83,8 +83,8 @@ void	draw_player(t_data *data)
 
 	p_size = 10;
 	//printf("Player position: x=%.2f, y=%.2f\n", data->player->p_x, data->player->p_y);
-	centered_x = (int)((data->player->p_x * SIZE) + (SIZE / 2) - (p_size / 2));
-	centered_y = (int)((data->player->p_y * SIZE) + (SIZE / 2) - (p_size / 2));
+	centered_x = (int)((data->player->p_x * SIZE) - (p_size / 2));
+	centered_y = (int)((data->player->p_y * SIZE) - (p_size / 2));
 	//printf("Drawing at: x=%d, y=%d\n", centered_x, centered_y);
 	draw_square(data, centered_x, centered_y, p_size, 0x2768F5);
 }
