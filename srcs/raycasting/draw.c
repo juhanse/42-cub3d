f0f5904d6cbb5803f46fc3ab0817d1a8d74464e6
Antonia <a.dewoelmont@gmail.com>
2025-09-15 12:06:24 +0200

    fix: draw_mini cast int to char

--- srcs/raycasting/draw.c
+++ srcs/raycasting/draw.c
@@ -58,7 +58,7 @@ void	put_mini_pixel(t_data *data, int x, int y, int color)
 		data->mlx_img.data[index + 0] = (color >> 0) & 0xFF;
 		data->mlx_img.data[index + 1] = (color >> 8) & 0xFF;
 		data->mlx_img.data[index + 2] = (color >> 16) & 0xFF;
-		data->mlx_img.data[index + 3] = 255;
+		data->mlx_img.data[index + 3] = (char)255;
 	}
 	else
 		return ;
