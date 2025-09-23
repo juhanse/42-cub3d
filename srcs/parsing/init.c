891b8015a34286de5fc23050c264827547763681
juhanse <julien.hanse12@gmail.com>
2025-09-23 22:02:06 +0200

    fix: get max width

--- srcs/parsing/init.c
+++ srcs/parsing/init.c
@@ -13,6 +13,22 @@ static int	ft_check_path(char *path)
 	return (0);
 }
 
+static void	ft_get_max_width(t_data *data)
+{
+	int	i;
+	int	len;
+
+	i = -1;
+	len = 0;
+	data->map_max_width = (int)ft_strlen(data->map[0]);
+	while (data->map[++i])
+	{
+		len = (int)ft_strlen(data->map[i]);
+		if (len > data->map_max_width)
+			data->map_max_width = len;
+	}
+}
+
 int	ft_initialize(t_data *data, char *path)
 {
 	if (ft_check_path(path))
@@ -26,10 +42,13 @@ int	ft_initialize(t_data *data, char *path)
 		return (free(data->player), perror(ERR_MALLOC), 1);
 	if (!ft_fill_content(data))
 		return (free(data->player), free(data->minimap), 1);
+	data->ceiling_color = -1;
+	data->floor_color = -1;
 	if (!ft_get_config_texture(data) || !ft_get_config_color(data))
 		return (ft_free_map(data, 1), perror(ERR_CONFIG), 1);
 	if (!ft_fill_map(data))
 		return (ft_free_map(data, 2), perror(ERR_MAP), 1);
+	ft_get_max_width(data);
 	if (!ft_found_player(data))
 		return (ft_free_map(data, 3), perror(ERR_PLY_NOT_FOUND), 1);
 	if (!ft_test_map(data))
